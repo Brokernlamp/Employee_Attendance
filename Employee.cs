fb339d0ca1c620e64db405abc81e705c18505656
Swar Jadhav <126198619+Brokernlamp@users.noreply.github.com>
2025-04-12 22:54:31 +0530

    Create Employee.cs

---
+++ Employee.cs
@@ -0,0 +1,10 @@
+
+public class Employee
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double BasicSalary { get; set; }
+    public int PresentDays { get; set; }
+    public int AbsentDays { get; set; }
+}
+
